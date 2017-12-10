a464b769fd4e68cc64c173be7013ba1cd105711b
Osman Emara <emaraosman@gmail.com>
2017-12-10 17:25:27 -0500

    fix Statistic model/migration, to proper columns

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,15 +1,2 @@
 class Statistic < ApplicationRecord
-  def change
-    create_table :statistics do |t|
-      t.integer :sleep
-      t.integer :happiness
-      t.integer :energy
-      t.integer :stress
-      t.integer :optimism
-      t.text :wins
-      t.text :losses
-      t.belongs_to :user_id, index: true
-      t.timestamps
-    end
-  end
 end
--- db/migrate/20171210220156_create_statistics.rb
+++ db/migrate/20171210220156_create_statistics.rb
@@ -1,7 +1,14 @@
 class CreateStatistics < ActiveRecord::Migration[5.1]
   def change
     create_table :statistics do |t|
-
+      t.integer :sleep
+      t.integer :happiness
+      t.integer :energy
+      t.integer :stress
+      t.integer :optimism
+      t.text :wins
+      t.text :losses
+      t.belongs_to :user_id, index: true
       t.timestamps
     end
   end
